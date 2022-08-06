4ba03f47b3d6fb95f1ce8091cdf219ae50bc3394
zyansheep <zyansheep@protonmail.com>
2022-08-05 23:53:43 -0400

    test out invoke command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,13 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![hello_world])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+
+#[tauri::command]
+fn hello_world() {
+  println!("Hello World!");
 }
