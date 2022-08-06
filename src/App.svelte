23ac6f3c70e0d1b939eb573561bc71fb569942a8
zyansheep <zyansheep@protonmail.com>
2022-08-06 09:56:23 -0400

    yay graph works!

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Counter from './lib/Counter.svelte'
+	import Graph from './lib/Graph.svelte'
   import { invoke } from '@tauri-apps/api/tauri'
 
   function say_hello() {
@@ -10,14 +11,20 @@
 <main>
   <div>
   </div>
-  <h1>Vite + Svelte YAY!</h1>
+  <h1>Vite + Svelte YAY Hello!</h1>
 
   <div class="card">
     <Counter />
   </div>
+	<div class="graph">
+		<Graph />
+	</div>
   <button on:click="{say_hello}">Hello World!</button>
 </main>
 
 <style>
-
+	.graph {
+		width: 200;
+		height: 200;
+	}
 </style>
---
+++ src/lib/Graph.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { DataSet, Network } from "vis-network/standalone"
+	import { onMount } from 'svelte';
+	/* export let nodes: vis.DataSetNodes;
+	export let edges; */
+	var nodes = new DataSet([
+		{ id: 1, label: "Node 1" },
+		{ id: 2, label: "Node 2" },
+		{ id: 3, label: "Node 3" },
+		{ id: 4, label: "Node 4" },
+		{ id: 5, label: "Node 5" },
+	]);
+
+	// create an array with edges
+	var edges = [
+		{ from: 1, to: 3 },
+		{ from: 1, to: 2 },
+		{ from: 2, to: 4 },
+		{ from: 2, to: 5 },
+		{ from: 3, to: 3 },
+	];
+
+	let container;
+	
+	onMount(async () => {
+		var data = {
+			nodes,
+			edges,
+		};
+		var options = {};
+		var network = new Network(container, data, options);
+	});
+</script>
+
+<style>
+	#mynetwork {
+		width: 600px;
+		height: 400px;
+		border: 1px solid lightgray;
+	}
+</style>
+
+<div id="mynetwork" bind:this={container}></div>
